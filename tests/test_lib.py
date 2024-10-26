6ed9a594159cb06c4bea47aa7ca6309ee9486fea
John C <john.ch.fr@gmail.com>
2024-10-26 13:16:41 +0530

    add type-hints and type-check in CI

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -13,18 +13,18 @@ from apple_hdr_heic import (
 DATA_DIR = pathlib.Path(__file__).parent.absolute() / "data"
 
 
-def test_quantize():
-    arr1 = np.array([0.0, 0.1, 0.9, 1.0])
+def test_quantize() -> None:
+    arr1 = np.array([0.0, 0.1, 0.9, 1.0], dtype=np.float32)
     qarr1 = quantize_to_uint16(arr1)
     assert np.all(qarr1 == np.array([0, 6554, 58982, 0xFFFF]))
     assert qarr1.dtype == np.uint16
 
-    arr2 = np.array([-0.5, 2.0])
+    arr2 = np.array([-0.5, 2.0], dtype=np.float32)
     qarr2 = quantize_to_uint16(arr2)
     assert np.all(qarr2 == np.array([0, 0xFFFF]))
 
 
-def test_combine():
+def test_combine() -> None:
     test_sdr = np.linspace(0., 1.0, num=12, dtype=np.float32).reshape(2, 2, 3)
     test_gainmap = np.linspace(0., 1.0, num=4, dtype=np.float32).reshape(2, 2)
     headroom = 4.0
@@ -38,7 +38,7 @@ def test_combine():
     assert np.all(quant_combined == qc_expected)
 
 
-def test_dp3_to_bt2020():
+def test_dp3_to_bt2020() -> None:
     dp3 = np.array([[[0, 0, 0], [1, 1, 1]],
                     [[2, 0, 0], [0, 2, 2]]], dtype=np.float32)
     bt2020 = displayp3_to_bt2020(dp3)
@@ -49,14 +49,14 @@ def test_dp3_to_bt2020():
     assert np.allclose(bt2020, bt2020_expected)
 
 
-def test_load_and_combine():
+def test_load_and_combine() -> None:
     dp3_hdr = load_and_combine_gainmap(DATA_DIR / "hdr-sample.heic")
     assert np.all(dp3_hdr >= 0.0)
     assert np.all(dp3_hdr <= 7.38)  # note: 7.3717 is the headroom
     assert dp3_hdr.dtype == np.float32
 
 
-def test_load_as_bt2100_pq():
+def test_load_as_bt2100_pq() -> None:
     bt2100_pq = load_as_bt2100_pq(DATA_DIR / "hdr-sample.heic")
     assert np.all(bt2100_pq >= 0.0)
     assert np.all(bt2100_pq <= 0.8)
