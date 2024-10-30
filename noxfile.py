074bee981078b3433e1795b52655c068846ee1b6
John C <john.ch.fr@gmail.com>
2024-10-30 23:10:47 +0530

    update to use uv and nox

---
+++ noxfile.py
@@ -0,0 +1,30 @@
+import nox
+
+nox.options.default_venv_backend = "uv|virtualenv"
+
+
+@nox.session
+def test(session):
+    session.install(".")
+    session.install("pytest")
+    session.run("pytest")
+
+
+@nox.session
+def typeck(session):
+    session.install(".")
+    session.install("mypy")
+    session.run("mypy", "src", "tests")
+
+
+@nox.session
+def lint(session):
+    session.install("ruff")
+    session.run("ruff", "check", ".")
+    session.run("ruff", "check", "--select", "I", ".")
+
+
+@nox.session
+def style(session):
+    session.install("ruff")
+    session.run("ruff", "format", "--diff", ".")
