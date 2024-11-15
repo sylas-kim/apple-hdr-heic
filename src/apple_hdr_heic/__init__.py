2da8a69372759648660acf1d776eb27148d2d60e
John C <john.ch.fr@gmail.com>
2024-11-15 21:36:34 +0530

    refactor lib

--- src/apple_hdr_heic/__init__.py
+++ src/apple_hdr_heic/__init__.py
@@ -1,17 +1,17 @@
 from .lib import (
-    combine_hdrgainmap,
+    apply_hdrgainmap,
     displayp3_to_bt2020,
-    load_and_combine_gainmap,
     load_as_bt2100_pq,
+    load_as_displayp3_linear,
     quantize_to_uint16,
 )
 from .metadata import AppleHDRMetadata
 
 __all__ = [
     "AppleHDRMetadata",
-    "combine_hdrgainmap",
+    "apply_hdrgainmap",
     "displayp3_to_bt2020",
-    "load_and_combine_gainmap",
     "load_as_bt2100_pq",
+    "load_as_displayp3_linear",
     "quantize_to_uint16",
 ]
