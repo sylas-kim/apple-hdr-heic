0ee8b5173e24bd639ec20fbd002d71690acb1db9
John C <john.ch.fr@gmail.com>
2024-12-09 15:37:31 +0530

    add more decoder tests

--- tests/test_decoder.py
+++ tests/test_decoder.py
@@ -1,6 +1,8 @@
 import subprocess
+from pathlib import Path
 
 ENTRYPOINT = "apple-hdr-heic-decoder"
+TEST_FILE = Path(__file__).parent / "data/hdr-sample.heic"
 
 
 def test_help():
@@ -8,4 +10,42 @@ def test_help():
     assert subp.returncode == 0
 
 
-# TODO test export for each file type
+def _output_format_test(output_format: str, q=None, b=None, y=None, colourspace=None):
+    assert output_format in {"png", "heic", "avif", "exr"}
+    outfile = Path(f"/tmp/hdr-out.{output_format}")
+    assert not outfile.exists()
+    args = [ENTRYPOINT, TEST_FILE, outfile]
+    if q is not None:
+        args.extend(["-q", str(q)])
+    if b is not None:
+        args.extend(["-b", str(b)])
+    if y is not None:
+        args.extend(["-y", str(y)])
+    if colourspace is not None:
+        args.extend(["--colourspace", colourspace])
+    subp = subprocess.run(args)
+    assert subp.returncode == 0
+    assert outfile.exists()
+    outfile.unlink()
+
+
+def test_png():
+    _output_format_test("png")
+
+
+def test_heic():
+    _output_format_test("heic")
+    _output_format_test("heic", q=99, b=10, y=422)
+    _output_format_test("heic", q=100, b=12, y=444)
+
+
+def test_avif():
+    _output_format_test("avif")
+    _output_format_test("avif", q=99, b=10, y=422)
+    _output_format_test("avif", q=100, b=12, y=444)
+
+
+def test_exr():
+    _output_format_test("exr")
+    _output_format_test("exr", b=16, colourspace="ROMM RGB")
+    _output_format_test("exr", b=32, colourspace="Display P3")
